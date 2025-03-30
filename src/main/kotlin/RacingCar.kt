0929a40374234766b606cd44b41dbd53f3f04f03
Kwoun Ki Ho <fingercut3822@gmail.com>
2025-03-30 17:43:07 +0900

    refactor: 함수 분리

--- src/main/kotlin/RacingCar.kt
+++ src/main/kotlin/RacingCar.kt
@@ -10,20 +10,39 @@ class RacingCar {
     private val inputView = InputView()
 
     fun run() {
-        outputView.promptForCarNames()
-        val carNames = inputView.readCarNames()
-        outputView.promptForRaceCount()
-        val attemptCount = inputView.readAttemptCount()
+        val carNames = readCarNames()
+        val attemptCount = readAttemptCount()
 
         val cars = carNames.map { Car(it) }
         val game = RacingGame(cars, RandomNumberGenerator())
 
+        processGame(attemptCount, game, cars)
+        printWinners(cars)
+    }
+
+    private fun readCarNames(): List<String> {
+        outputView.promptForCarNames()
+        return inputView.readCarNames()
+    }
+
+    private fun readAttemptCount(): Int {
+        outputView.promptForRaceCount()
+        return inputView.readAttemptCount()
+    }
+    
+    private fun processGame(
+        attemptCount: Int,
+        game: RacingGame,
+        cars: List<Car>
+    ) {
         repeat(attemptCount) {
             game.play()
             val scores = CarScores(cars)
             outputView.printScores(scores.getScores())
         }
+    }
 
+    private fun printWinners(cars: List<Car>) {
         val scores = CarScores(cars)
         outputView.printWinners(scores.findWinners())
     }
