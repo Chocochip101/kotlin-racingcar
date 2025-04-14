84e922db5c51c70fc41a242eaacc5c25a7d954f7
Kwoun Ki Ho <fingercut3822@gmail.com>
2025-04-14 15:37:00 +0900

    refactor: carName data class로 변경

--- src/main/kotlin/racinggame/CarName.kt
+++ src/main/kotlin/racinggame/CarName.kt
@@ -1,6 +1,6 @@
 package racinggame
 
-class CarName(val value: String) {
+data class CarName(val value: String) {
     init {
         require(value.length <= CAR_NAME_LIMIT) { ERROR_MESSAGE }
     }
