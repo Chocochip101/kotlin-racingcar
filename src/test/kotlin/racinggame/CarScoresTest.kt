a1a6c3e24b358d01bfab55331a6482e5c92a638a
Kwoun Ki Ho <fingercut3822@gmail.com>
2025-04-17 00:15:47 +0900

    test: 픽스처 사용하게 변경

--- src/test/kotlin/racinggame/CarScoresTest.kt
+++ src/test/kotlin/racinggame/CarScoresTest.kt
@@ -1,5 +1,6 @@
 package racinggame
 
+import fixture.CarFixture
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContainAll
@@ -9,51 +10,47 @@ import io.kotest.matchers.shouldBe
 
 class CarScoresTest : StringSpec({
 
-    val name1 = CarName("eden")
-    val name2 = CarName("clove")
-    val name3 = CarName("lini")
-
-    lateinit var cars: List<Car>
-    lateinit var carScores: CarScores
-
-    beforeTest {
-        cars =
-            listOf(
-                Car(name1, 3),
-                Car(name2, 5),
-                Car(name3, 5),
-            )
-        carScores = CarScores(cars)
-    }
-
     "우승자가 2명일 때, 두 명이 우승자로 반환되어야 한다." {
+        val car1 = CarFixture.edenCar
+        val car2 = CarFixture.liniCar
+        val car3 = CarFixture.cloveCar
+        car1.move()
+        car2.move()
+        val cars = listOf(car1, car2, car3)
+        val carScores = CarScores(cars)
+
         val winners = carScores.findWinners()
 
         winners.shouldHaveSize(2)
-        winners.shouldContainAll(name2.value, name3.value)
+        winners.shouldContainAll(car1.getName(), car2.getName())
     }
 
     "우승자가 1명일 때, 한 명만 우승자로 반환되어야 한다." {
-        val singleWinnerCars =
-            listOf(
-                Car(name1, 3),
-                Car(name2, 4),
-                Car(name3, 2),
-            )
+        val car1 = CarFixture.edenCar
+        val car2 = CarFixture.liniCar
+        val car3 = CarFixture.cloveCar
+        car1.move()
+        val singleWinnerCars = listOf(car1, car2, car3)
         val singleWinnerScores = CarScores(singleWinnerCars)
 
         val winners = singleWinnerScores.findWinners()
 
         winners.shouldHaveSize(1)
-        winners[0] shouldBe name2.value
+        winners[0] shouldBe car1.getName()
     }
 
     "각 자동차의 점수를 확인할 때, 자동차 이름에 맞는 점수가 반환되어야 한다." {
+        val car1 = CarFixture.edenCar
+        val car2 = CarFixture.liniCar
+        val car3 = CarFixture.cloveCar
+        car3.move()
+        val cars = listOf(car1, car2, car3)
+        val carScores = CarScores(cars)
         val scores = carScores.getScores()
 
-        scores.shouldContain(name1.value to 3)
-        scores.shouldContain(name2.value to 5)
-        scores.shouldContain(name3.value to 5)
+        scores.shouldContain(car1.getName() to 0)
+        scores.shouldContain(car2.getName() to 0)
+        scores.shouldContain(car3.getName() to 1)
     }
 
     "자동차가 없을 때, findWinners는 예외를 던져야 한다." {
